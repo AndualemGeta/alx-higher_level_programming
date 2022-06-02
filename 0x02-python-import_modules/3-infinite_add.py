d67bbdfe1e8b4327ae5d80f5c27e4edd18b03681
AndualemGeta <andualemgetachew80@gmail.com>
2022-06-02 11:47:36 -0700

    last commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print(0)
-    elif len(sys.argv) == 2:
-        print(sys.argv[1])
-    else:
-        sum = 0
-        for i in range(1, len(sys.argv)):
-            a = int(sys.argv[i])
-            sum = sum + a
-            print(sum)
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
